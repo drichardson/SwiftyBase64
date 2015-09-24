9b0962860ecd555229812d35b2b318eed2b99846
Doug Richardson <dougie.richardson@gmail.com>
2015-09-24 11:24:09 -0700

    Swift 2 conversion. Use array with running index instead of slices, since in Swift 2 they are no longer 0 indexed and it made the code more complicated.

--- SwiftyBase64/Base64.swift
+++ SwiftyBase64/Base64.swift
@@ -23,9 +23,9 @@ public enum Alphabet {
 /**
     Encode a [UInt8] byte array as a Base64 String.
 
-    :param: bytes Bytes to encode.
-    :param: alphabet The Base64 alphabet to encode with.
-    :returns: A String of the encoded bytes.
+    - parameter bytes: Bytes to encode.
+    - parameter alphabet: The Base64 alphabet to encode with.
+    - returns: A String of the encoded bytes.
 */
 public func EncodeString(bytes : [UInt8], alphabet : Alphabet = .Standard) -> String {
     let encoded = Encode(bytes, alphabet : alphabet)
@@ -54,24 +54,24 @@ private func tableForAlphabet(alphabet : Alphabet) -> [UInt8] {
     - Standard (section 4)
     - URL and Filename Safe (section 5)
 
-    :param: bytes Bytes to encode.
-    :param: alphabet The Base64 alphabet to encode with.
-    :returns: Base64 encoded ASCII bytes.
+    - parameter bytes: Bytes to encode.
+    - parameter alphabet: The Base64 alphabet to encode with.
+    - returns: Base64 encoded ASCII bytes.
 */
 public func Encode(bytes : [UInt8], alphabet : Alphabet = .Standard) -> [UInt8] {
     var encoded : [UInt8] = []
     
-    var b = bytes[0..<bytes.count]
-    
-    //
     let table = tableForAlphabet(alphabet)
     let padding = table[64]
     
-    for ; b.count >= 3; b = b[3..<b.count] {
-        let one = b[0] >> 2
-        let two = ((b[0] & 0b11) << 4) | ((b[1] & 0b11110000) >> 4)
-        let three = ((b[1] & 0b00001111) << 2) | ((b[2] & 0b11000000) >> 6)
-        let four = b[2] & 0b00111111
+    var i = 0
+    let count = bytes.count
+    
+    for ; i+3 <= count; i += 3 {
+        let one = bytes[i] >> 2
+        let two = ((bytes[i] & 0b11) << 4) | ((bytes[i+1] & 0b11110000) >> 4)
+        let three = ((bytes[i+1] & 0b00001111) << 2) | ((bytes[i+2] & 0b11000000) >> 6)
+        let four = bytes[i+2] & 0b00111111
         
         encoded.append(table[Int(one)])
         encoded.append(table[Int(two)])
@@ -79,23 +79,23 @@ public func Encode(bytes : [UInt8], alphabet : Alphabet = .Standard) -> [UInt8] 
         encoded.append(table[Int(four)])
     }
     
-    if b.count == 2 {
+    if i+2 == count {
         // (3) The final quantum of encoding input is exactly 16 bits; here, the
         // final unit of encoded output will be three characters followed by
         // one "=" padding character.
-        let one = b[0] >> 2
-        let two = ((b[0] & 0b11) << 4) | ((b[1] & 0b11110000) >> 4)
-        let three = ((b[1] & 0b00001111) << 2)
+        let one = bytes[i] >> 2
+        let two = ((bytes[i] & 0b11) << 4) | ((bytes[i+1] & 0b11110000) >> 4)
+        let three = ((bytes[i+1] & 0b00001111) << 2)
         encoded.append(table[Int(one)])
         encoded.append(table[Int(two)])
         encoded.append(table[Int(three)])
         encoded.append(padding)
-    } else if b.count == 1 {
+    } else if i+1 == count {
         // (2) The final quantum of encoding input is exactly 8 bits; here, the
         // final unit of encoded output will be two characters followed by
         // two "=" padding characters.
-        let one = b[0] >> 2
-        let two = ((b[0] & 0b11) << 4)
+        let one = bytes[i] >> 2
+        let two = ((bytes[i] & 0b11) << 4)
         encoded.append(table[Int(one)])
         encoded.append(table[Int(two)])
         encoded.append(padding)
@@ -104,7 +104,7 @@ public func Encode(bytes : [UInt8], alphabet : Alphabet = .Standard) -> [UInt8] 
         // (1) The final quantum of encoding input is an integral multiple of 24
         // bits; here, the final unit of encoded output will be an integral
         // multiple of 4 characters with no "=" padding.
-        assert(b.count == 0)
+        assert(i == count)
     }
     
     return encoded
