e0fbd2d12881388158ba159000b430be8803d875
Dania Popov <daniapopov@gmail.com>
2016-10-06 17:09:11 -0400

    Swift 3
    Fixed previous problems and updated Tests

--- SwiftyBase64/Base64.swift
+++ SwiftyBase64/Base64.swift
@@ -7,28 +7,28 @@
 //
 
 /**
-    Base64 Alphabet to use during encoding.
-
-    - Standard: The standard Base64 encoding, defined in RFC 4648 section 4.
-    - URLAndFilenameSafe: The base64url encoding, defined in RFC 4648 section 5.
-*/
+ Base64 Alphabet to use during encoding.
+ 
+ - Standard: The standard Base64 encoding, defined in RFC 4648 section 4.
+ - URLAndFilenameSafe: The base64url encoding, defined in RFC 4648 section 5.
+ */
 public enum Alphabet {
     /// The standard Base64 alphabet
-    case Standard
+    case standard
     
     /// The URL and Filename Safe Base64 alphabet
-    case URLAndFilenameSafe
+    case urlAndFilenameSafe
 }
 
 /**
-    Encode a [UInt8] byte array as a Base64 String.
-
-    - parameter bytes: Bytes to encode.
-    - parameter alphabet: The Base64 alphabet to encode with.
-    - returns: A String of the encoded bytes.
-*/
-public func EncodeString(bytes : [UInt8], alphabet : Alphabet = .Standard) -> String {
-    let encoded = Encode(bytes: bytes, alphabet : alphabet)
+ Encode a [UInt8] byte array as a Base64 String.
+ 
+ - parameter bytes: Bytes to encode.
+ - parameter alphabet: The Base64 alphabet to encode with.
+ - returns: A String of the encoded bytes.
+ */
+public func EncodeString(_ bytes : [UInt8], alphabet : Alphabet = .standard) -> String {
+    let encoded = Encode(bytes, alphabet : alphabet)
     var result = String()
     for b in encoded {
         result.append(String(UnicodeScalar(b)))
@@ -37,48 +37,48 @@ public func EncodeString(bytes : [UInt8], alphabet : Alphabet = .Standard) -> St
 }
 
 /// Get the encoding table for the alphabet.
-private func tableForAlphabet(alphabet : Alphabet) -> [UInt8] {
+private func tableForAlphabet(_ alphabet : Alphabet) -> [UInt8] {
     switch alphabet {
-    case .Standard:
+    case .standard:
         return StandardAlphabet
-    case .URLAndFilenameSafe:
+    case .urlAndFilenameSafe:
         return URLAndFilenameSafeAlphabet
     }
 }
 
 /**
-    Use the Base64 algorithm as decribed by RFC 4648 section 4 to
-    encode the input bytes. The alphabet specifies the translation
-    table to use. RFC 4648 defines two such alphabets:
-
-    - Standard (section 4)
-    - URL and Filename Safe (section 5)
-
-    - parameter bytes: Bytes to encode.
-    - parameter alphabet: The Base64 alphabet to encode with.
-    - returns: Base64 encoded ASCII bytes.
-*/
-public func Encode(bytes : [UInt8], alphabet : Alphabet = .Standard) -> [UInt8] {
+ Use the Base64 algorithm as decribed by RFC 4648 section 4 to
+ encode the input bytes. The alphabet specifies the translation
+ table to use. RFC 4648 defines two such alphabets:
+ 
+ - Standard (section 4)
+ - URL and Filename Safe (section 5)
+ 
+ - parameter bytes: Bytes to encode.
+ - parameter alphabet: The Base64 alphabet to encode with.
+ - returns: Base64 encoded ASCII bytes.
+ */
+public func Encode(_ bytes : [UInt8], alphabet : Alphabet = .standard) -> [UInt8] {
     var encoded : [UInt8] = []
     
-    let table = tableForAlphabet(alphabet: alphabet)
+    let table = tableForAlphabet(alphabet)
     let padding = table[64]
     
+    var i = 0
     let count = bytes.count
     
-    var i = 0
-    
-    for c in stride(from: 0,to:count, by: 3) {
-        let one = bytes[c] >> 2
-        let two = ((bytes[c] & 0b11) << 4) | ((bytes[c+1] & 0b11110000) >> 4)
-        let three = ((bytes[c+1] & 0b00001111) << 2) | ((bytes[c+2] & 0b11000000) >> 6)
-        let four = bytes[c+2] & 0b00111111
+    while i+3 <= count {
+        let one = bytes[i] >> 2
+        let two = ((bytes[i] & 0b11) << 4) | ((bytes[i+1] & 0b11110000) >> 4)
+        let three = ((bytes[i+1] & 0b00001111) << 2) | ((bytes[i+2] & 0b11000000) >> 6)
+        let four = bytes[i+2] & 0b00111111
         
         encoded.append(table[Int(one)])
         encoded.append(table[Int(two)])
         encoded.append(table[Int(three)])
         encoded.append(table[Int(four)])
-        i = c
+        
+        i += 3
     }
     
     if i+2 == count {
@@ -108,6 +108,6 @@ public func Encode(bytes : [UInt8], alphabet : Alphabet = .Standard) -> [UInt8] 
         // multiple of 4 characters with no "=" padding.
         assert(i == count)
     }
-    
+        
     return encoded
 }
